896b5cb08db842c2d1701f96ae0a95b967eee05b
Ahmet CAN <cnahmet@outlook.com>
2021-02-25 15:37:34 +0300

    day 6

---
+++ day6-0.js
@@ -0,0 +1,48 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+function getMaxLessThanK(n, k) {
+    let max_anb = 0;
+    for (let b = n; b > 0; b--) {
+        for (let a = b-1; a > 0; a--) {
+            if ((a & b) < k && (a & b) > max_anb){
+                max_anb = (a&b);
+            }
+        }
+    }
+    return max_anb;
+}
+
+function main() {
+    const q = +(readLine());
+    
+    for (let i = 0; i < q; i++) {
+        const [n, k] = readLine().split(' ').map(Number);
+        
+        console.log(getMaxLessThanK(n, k));
+    }
+}
+
+/*
+Bitwise operatörü
+*/
