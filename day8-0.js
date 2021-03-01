2020f3c04dc62b71cfd49523888218b2190cc668
Ahmet CAN <cnahmet@outlook.com>
2021-03-01 12:46:17 +0300

    last

---
+++ day8-0.js
@@ -0,0 +1,40 @@
+/* HTML
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Button</title>
+        <link rel="stylesheet" href="css/button.css" type="text/css">
+    </head>
+
+    <body>
+        <script src="js/button.js" type="text/javascript"></script>
+    </body>
+</html>
+
+*/
+
+/* CSS
+
+#btn {
+    width: 96px;
+    height: 48px;
+    font-size: 24px;
+}
+
+*/
+
+/* JS
+
+var btn = document.createElement("Button");
+
+btn.innerHTML = "0";
+btn.id = "btn";
+document.body.appendChild(btn);
+
+btn.onclick = function() {
+    btn.innerHTML++;
+}
+
+*/
